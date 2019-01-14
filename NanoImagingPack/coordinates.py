Bug fixes and introduced a number of new functions. DampEdges was updated and by default now does dim down to the average. Also the "moison" method was added.

NanoImagingPack/coordinates.py
@@ -1,7 +1,7 @@
 import numpy as np;
 from .util import get_type;
 
-def ramp(mysize=(256,256), ramp_dim=0, corner='center'):
+def ramp(mysize=(256,256), ramp_dim=0, corner='center', freq=None):
     '''
     creates a ramp in the given direction direction 
     standart size is 256 X 256
@@ -11,7 +11,8 @@ def ramp(mysize=(256,256), ramp_dim=0, corner='center'):
                     e.g. size_x = 100 -> goes from -50 to 49
                          size_x = 101 -> goes from -50 to 50
         negative : goes from negative size_x to 0
-        positvie : goes from 0 size_x to positive
+        positive : goes from 0 size_x to positive
+        freq : if "freq" is given, the Fourier-space frequency scale (roughly -0.5 to 0.5) is used.
         int number: is the index where the center is!
     '''
     from .image import image;
@@ -39,11 +40,15 @@ def ramp(mysize=(256,256), ramp_dim=0, corner='center'):
     minisize[ramp_dim] = mysize[ramp_dim];
     #np.seterr(divide ='ignore');
     miniramp = np.reshape(miniramp,minisize)
-    res*=miniramp;
+    if freq=="freq":
+        res*=(miniramp/(mysize[ramp_dim]//2)/2.0)
+    elif freq=="radfreq":
+        res*=(miniramp/(mysize[ramp_dim]//2/2.0)*2*np.pi)
+    else:
+        res*=miniramp
     return(image(res));  # RH casted to image
 
-
-def xx(mysize = (256,256), mode = 'center'):
+def xx(mysize = (256,256), mode = 'center', freq=None):
     '''
     creates a ramp in x direction 
     standart size is 256 X 256
@@ -54,9 +59,9 @@ def xx(mysize = (256,256), mode = 'center'):
         negative : goes from negative size_x to 0
         positvie : goes from 0 size_x to positive
     '''
-    return(ramp(mysize,0,mode))
+    return(ramp(mysize,0,mode,freq))
 
-def yy(mysize = (256,256), mode = 'center'):
+def yy(mysize = (256,256), mode = 'center', freq=None):
     '''
     creates a ramp in y direction 
     standart size is 256 X 256
@@ -67,9 +72,9 @@ def yy(mysize = (256,256), mode = 'center'):
         negative : goes from negative size_y to 0
         positvie : goes from 0 size_y to positive
     '''
-    return(ramp(mysize,1,mode))
+    return(ramp(mysize,1,mode,freq))
  
-def zz(mysize = (256,256), mode = 'center'):
+def zz(mysize = (256,256), mode = 'center', freq=None):
     '''
     creates a ramp in z direction 
     standart size is 256 X 256
@@ -82,7 +87,7 @@ def zz(mysize = (256,256), mode = 'center'):
     '''
     return(ramp(mysize,2,mode))
 
-def rr2(mysize=(256,256), offset = (0,0),scale = None):
+def rr2(mysize=(256,256), offset = (0,0),scale = None, freq=None):
     '''
     creates a square of a ramp in r direction 
     standart size is 256 X 256
@@ -108,12 +113,12 @@ def rr2(mysize=(256,256), offset = (0,0),scale = None):
         scale = list(scale[0:2]);
     else:
         raise TypeError('Wrong data type for scale -> must be integer, list, tuple or none')
-    res=((ramp(mysize,0,'center')-offset[0])*scale[0])**2
+    res=((ramp(mysize,0,'center',freq)-offset[0])*scale[0])**2
     for d in range(1,len(mysize)):
-        res+=((ramp(mysize,d,'center')-offset[d])*scale[d])**2
+        res+=((ramp(mysize,d,'center',freq)-offset[d])*scale[d])**2
     return res
 
-def rr(mysize=(256,256), offset = (0,0),scale = None):
+def rr(mysize=(256,256), offset = (0,0),scale = None, freq=None):
     '''
     creates a ramp in r direction 
     standart size is 256 X 256
@@ -121,7 +126,7 @@ def rr(mysize=(256,256), offset = (0,0),scale = None):
     offset -> x/y offset in pixels (number, list or tuple)
     scale is tuple, list, none or number of axis scale
     '''
-    return np.sqrt(rr2(mysize,offset,scale))
+    return np.sqrt(rr2(mysize,offset,scale,freq))
    
 def phiphi(mysize=(256,256), offset = 0, angle_range = 1):
     '''
@@ -145,9 +150,6 @@ def phiphi(mysize=(256,256), offset = 0, angle_range = 1):
     phi[phi.shape[0]//2,phi.shape[1]//2]=0;
     np.seterr(divide='warn', invalid = 'warn');
     return(phi)
-
-
-
 
 def VolumeList(MyShape = (256,256), MyCenter = 'center', MyStretch = 1, polar_axes = None, return_axes = 'all'):
         

NanoImagingPack/image.py
@@ -19,11 +19,13 @@ Created on Thu Jul 27 17:21:21 2017
 
 import tifffile as tif;
 import numpy as np;
+import NanoImagingPack as nip
 from pkg_resources import resource_filename
 from .config import DBG_MSG, __DEFAULTS__;
 from IPython.lib.pretty import pretty;
 from .functions import cossqr, gaussian, coshalf, linear;
 from .util import make_damp_ramp,get_type,subslice,expanddim;
+
 #class roi:
 #    def __init__(im):
 #    '''
@@ -237,7 +239,9 @@ class image(np.ndarray):
     
     def mid(self, ax = None):
         '''
-            returns the midpos of the given axis (as tuple)
+            returns the midpos of the given axis (as tuple) as seen for ft, i.e. im.shape//2
+            ----
+            ax : which axes (list of all axes)
             If nothing given, it returns the mid pos for all axis
         '''
         import numbers;
@@ -782,6 +786,7 @@ def DampEdge(im, width = None, rwidth=0.1, axes =None, func = coshalf, method="d
         method -> which method should be used?
                 -> "zero" : dims down to zero
                 -> "damp" : blurs to an averaged mean (default)
+                -> "moisan" : HF/LF split method according to Moisan, J Math Imaging Vis (2011) 39: 161–179, DOI 10.1007/s10851-010-0227-1
     
         return image with damped edges
         
@@ -802,29 +807,48 @@ def DampEdge(im, width = None, rwidth=0.1, axes =None, func = coshalf, method="d
         width.extend(list(ext.astype(int)));
         
     res=im
+    mysum=im*0.0
+    sz=im.shape;
+    den=-2*len(set(axes)); # use only the counting dimensions
     for i in range(len(im.shape)):
         if i in axes:
             line = np.arange(0,im.shape[i],1);
-            ramp = make_damp_ramp(width[i],func);
+            ramp = make_damp_ramp(width[i],func);            
             if method=="zero":
                 line = cat((ramp[::-1],np.ones(im.shape[i]-2*width[i]),ramp),0);
                 goal=0.0 # dim down to zero
-            else:
+            elif method=="moisan":
+#                for d=1:ndims(img)
+                top=nip.subslice(im,i,0)
+                bottom=nip.subslice(im,i,-1)
+                mysum=nip.subsliceAsg(mysum,i,0,bottom-top + nip.subslice(mysum,i,0));
+                mysum=nip.subsliceAsg(mysum,i,sz[i]-1,top-bottom + nip.subslice(mysum,i,sz[i]-1));
+                den=den+2*np.cos(2*np.pi*nip.ramp(nip.dimVec(i,sz[i],len(sz)),i,freq='freq'))
+            elif method=="damp":
                 line = cat((ramp[::-1],np.ones(im.shape[i]-2*width[i]+1),ramp[:-1]),0);  # to make it perfectly cyclic
-                top=subslice(im,i,0)
-                bottom=subslice(im,i,-1)
+                top=nip.subslice(im,i,0)
+                bottom=nip.subslice(im,i,-1)
                 goal = (top+bottom)/2.0
                 kernel=gaussian(goal.shape,sigma)
                 goal = convolve(goal,kernel,norm2nd=True)
+            else:
+                raise ValueError("DampEdge: Unknown method. Choose: damp, moisan or zero.")
             #res = res.swapaxes(0,i); # The broadcasting works only for Python versions >3.5
 #            res = res.swapaxes(len(im.shape)-1,i); # The broadcasting works only for Python versions >3.5
-            line = expanddim(line,im.ndim).swapaxes(0,i); # The broadcasting works only for Python versions >3.5
-            try:
-                res = res*line + (1.0-line)*goal;
-            except ValueError:
-                print('Broadcasting failed! Maybe the Python version is too old ... - Now we have to use repmat and reshape :(')
-                from numpy.matlib import repmat;
-                res *= np.reshape(repmat(line, 1, np.prod(res.shape[1:])),res.shape, order = 'F');
+    if method=="moisan":
+        den=nip.MidValAsg(den,1);  # to avoid the division by zero error
+        den=nip.ft(mysum)/den;
+        den=nip.MidValAsg(den,0);  # kill the zero frequency
+        den=nip.ift(den)
+        res=im-den
+    else:
+        line = expanddim(line,im.ndim).swapaxes(0,i); # The broadcasting works only for Python versions >3.5
+        try:
+            res = res*line + (1.0-line)*goal
+        except ValueError:
+            print('Broadcasting failed! Maybe the Python version is too old ... - Now we have to use repmat and reshape :(')
+            from numpy.matlib import repmat;
+            res *= np.reshape(repmat(line, 1, np.prod(res.shape[1:])),res.shape, order = 'F');
         
     #return(res)
     return(res.view(image));