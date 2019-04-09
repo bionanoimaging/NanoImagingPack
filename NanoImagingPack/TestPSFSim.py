Bug fixes in pixelsize. Included Values, Units and scaling in v5.

NanoImagingPack/TestPSFSim.py
@@ -15,14 +15,20 @@ jincAperture = nip.jincAperture(obj,psfparams)
 h = nip.psf2d(obj,psfparams)
 
 obj3d = nip.readim("MITO_SIM")
-h = nip.psf(obj3d, psfparams)
 
-ObjTime=obj * np.exp(-nip.ramp1D(20, -4)/10)
+h = nip.psf(nip.zeros([100, 100, 100]), psfparams)
+
+# ObjTime=obj * np.exp(-nip.ramp1D(20, -4)/10)
 
 # v=v5(ObjTime)
 
 # v.NameElement(1,'HALLLOO')
 q = nip.catE(obj3d, - obj3d)
 
-v5(q)
-v5(obj3d)
+# v5(q)
+# v5(obj3d)
+
+q = nip.rr()
+q.pixelsize = [50,50]
+q.unit = ['nm','µm','m','s','ns']
+v = v5(q)

NanoImagingPack/coordinates.py
@@ -15,7 +15,7 @@ def unifysize(mysize):
 def ramp(mysize=(256, 256), ramp_dim=-1, placement='center', freq=None, shift=False, rftdir=-1, pxs=1.0):
     """
     creates a ramp in the given direction direction
-    standart size is 256 X 256
+    standard size is 256 X 256
     placement:
         center: 0 is at center
                 if x is even, it has one more value in the positve:
@@ -56,6 +56,7 @@ def ramp(mysize=(256, 256), ramp_dim=-1, placement='center', freq=None, shift=Fa
     res = util.ones(mysize)
     res *= myramp
 
+    res.pixelsize = util.expanddimvec(pxs,res.ndim,trailing=True)
     return res
 
 
@@ -116,7 +117,7 @@ def rr2(mysize=(256, 256), placement='center', offset=None, scale=None, freq=Non
     standart size is 256 X 256
     placement is always "center"
     offset -> x/y offset in pixels (number, list or tuple). It signifies the offset in pixel coordinates
-    scale is tuple, list, none or number. It defines the pixelsize .
+    scale is tuple, list, none or number. It defines the pixelsize.
     """
     import numbers
     mysize = list(unifysize(mysize))
@@ -140,11 +141,12 @@ def rr2(mysize=(256, 256), placement='center', offset=None, scale=None, freq=Non
     myplacement=placement
     if (type(placement) is list) or (type(placement) is np.array):
         myplacement=placement[0]
-    res = ((ramp(mysize, 0, myplacement, freq) - offset[0]) * scale[0]) ** 2
+#     res = ((ramp(mysize, 0, myplacement, freq) - offset[0]) * scale[0]) ** 2
+    res = ((ramp(mysize, 0, myplacement, freq, pxs=scale) - offset[0]) * scale[0]) ** 2
     for d in range(1, len(mysize)):
         if (type(placement) is list) or (type(placement) is np.array):
             myplacement = placement[d]
-        res += ((ramp(mysize, d, myplacement, freq) - offset[d]) * scale[d]) ** 2
+        res += ((ramp1D(mysize[d], d, myplacement, freq, pxs=scale[d]) - offset[d]) * scale[d]) ** 2
     return res
 
 
@@ -468,7 +470,8 @@ def ramp1D(mysize=256, ramp_dim=-1, placement='center', freq=None, pxs=1.0):
     elif ramp_dim < -1:
         miniramp = util.expanddim(miniramp, -ramp_dim,
                                   trailing=True)  # expands to this dimension numbe by inserting prevailing axes. Also converts to
-    return image(miniramp)
+    pxs = util.expanddimvec(pxs, miniramp.ndim, trailing=True)
+    return image(miniramp, pixelsize=pxs)
 
 
 def px_freq_step(im=(256, 256), pxs=62.5):

NanoImagingPack/image.py
@@ -23,7 +23,7 @@ import numbers
 import numpy as np
 from numpy.matlib import repmat
 from .FileUtils import list_files, get_sorted_file_list
-from os.path import join, isdir, splitext, isfile, split, isfile, join, splitext, split
+from os.path import join, isdir, splitext, isfile, split, isfile, join, splitext, split, basename
 from os import mkdir, listdir
 from scipy import misc
 from scipy.ndimage import rotate
@@ -207,6 +207,7 @@ def readim(path=None, which=None, pixelsize=None):
                 img.set_pixelsize(pixelsize)
             except OSError:
                 raise ValueError('No valid image file')
+        img.name = splitext(basename(path))[0]
         return img
     else:
         raise ValueError('No valid filename')
@@ -296,9 +297,9 @@ def readtimeseries(path, filename='', roi=[-1, -1, -1, -1], channel=0, ret_old_i
     print(str(number) + ' images read!')
 
     if ret_old_img_dim:
-        return image(final_im), max_im_dim
+        return image(final_im, name=splitext(basename(path))[0]), max_im_dim
     else:
-        return image(final_im)
+        return image(final_im, name=splitext(basename(path))[0])
 
 
 def gaussf(img, kernelSigma):
@@ -1467,8 +1468,8 @@ class image(np.ndarray):
         else:
             obj.im_number = 0
 
-        if name is None:
-            name = 'Img Nr'+str(obj.im_number)
+        # if name is None:
+        #     name = 'Img Nr'+str(obj.im_number)
 
         obj.spectral_axes =[]
         obj.ax_shifted = []
@@ -1524,6 +1525,8 @@ class image(np.ndarray):
                     elif (len(mysize) == 2) and (mysize[0] < 5) and (mysize[1] < 5):
                         print("np.image"+str(self))
                     else:
+                        # if self.name is None:
+                        #     self.name = util.caller_string(3)  # use the name of the caller
                         self.v = v5(self)
                 elif __DEFAULTS__['IMG_VIEWER'] == 'INFO':
                     print('Image :'+self.name)
@@ -1910,8 +1913,7 @@ class image(np.ndarray):
             self.im_number = max_im_number
         else:
             self.im_number = 0
-        self.name = 'Img Nr'+str(max_im_number)
-
+        # self.name = None # 'Img Nr'+str(max_im_number)
 
 
 def shiftby(img, avec):

NanoImagingPack/microscopy.py
@@ -192,7 +192,9 @@ def pupilRadius(im, psf_params = PSF_PARAMS):
         ft_pxs = px_freq_step(im, pxs)[-2:]
     shape = im.shape[-2:]
 
-    return rr(shape, scale=ft_pxs) * wl / NA  # [:2]
+    res = rr(shape, scale=ft_pxs) * wl / NA  # [:2]
+    res.pixelsize = im.pixelsize
+    return res
 
 def pupilAperture(im, psf_params = PSF_PARAMS):
     """
@@ -215,11 +217,13 @@ def jincAperture(im, psf_params = PSF_PARAMS):
     NA = psf_params.NA
     wl = psf_params.wavelength
     np.seterr(divide='ignore', invalid='ignore')
-    arg = rr(shape, scale=np.array(im.pixelsize) * 2 * np.pi * NA / wl)  # RH 3.2.19 was 2*mp.pi*np.sqrt((xx(shape)*self.pixelsize[0])**2+(yy(shape)*self.pixelsize[1])**2)*self.NA/self.wavelength;
+    arg = rr(shape, scale=np.array(im.pixelsize[-2:]) * 2 * np.pi * NA / wl)  # RH 3.2.19 was 2*mp.pi*np.sqrt((xx(shape)*self.pixelsize[0])**2+(yy(shape)*self.pixelsize[1])**2)*self.NA/self.wavelength;
+    arg.pixelsize = im.pixelsize[-2:]
     ret = 2 * j1(arg) / arg  # that again creates the dip!
     ret[shape[-2] // 2, shape[-1] // 2] = 1
     ret = ret / np.sum(np.abs(ret))
     plane = ft2d(ret)
+    plane.pixelsize = im.pixelsize
     np.seterr(divide='warn', invalid='warn')
     return plane
 
@@ -242,7 +246,7 @@ def cosSinAlpha(im, psf_params = PSF_PARAMS):
 
 def defocuPhase(cos_alpha, defocus=None, psf_params = PSF_PARAMS):
     """
-    caculates the complex-valued defocus phase pattern for a given focus position. The calculation is accurate for high NA. However, it does not consider Fourier-space undersampling effects.
+    calculates the complex-valued defocus phase propagation pattern for a given focus position. The calculation is accurate for high NA. However, it does not consider Fourier-space undersampling effects.
 
     :param defocus: real-space defocus value. if None is suzpplied, psf_params.off_focal_distance is used instead
     :param cos_alpha: a map of cos(alpha) values in the pupil plane. Use "cosSinAlpha(im,psf_param)" to optain such a map
@@ -299,7 +303,7 @@ def simLens(im, psf_params = PSF_PARAMS):
 
     # Make base field
     if psf_params.aperture_method == 'jinc':
-        plane = jincAperture(im,psf_params)
+        plane = jincAperture(im, psf_params)
     elif psf_params.aperture_method == 'hard':
         aperture = pupilAperture(im, psf_params)
         plane = aperture+0.0j
@@ -318,7 +322,7 @@ def simLens(im, psf_params = PSF_PARAMS):
     plane *= defocuPhase(cos_alpha)  # psf_params.off_focal_distance is used as default
 
     # expand to 4 Dims, the 4th will contain the electric fields
-    plane = plane.cat([plane,plane],-4)
+    plane = plane.cat([plane, plane], -4)
     plane.dim_description = {'d3': ['Ex','Ey','Ez']}
 
     # Apply vectorized distortions
@@ -372,7 +376,7 @@ def __make_transfer__(im, psf_params = PSF_PARAMS, mode = 'ctf', dimension = 2):
         ret /= np.sum(ret)
         ret = ret.ft2d(norm=None)
     ret.PSF_PARAMS = psf_params
-    return(ret)
+    return ret
 
 def otf(im, psf_params = PSF_PARAMS):
     """
@@ -1115,7 +1119,7 @@ def psf2d(im, psf_params = PSF_PARAMS):
             poly = np.random.rand(im.shape[-2], im.shape[-1])*np.exp(-2.546j);
             para.pol = [polx, poly]
     """
-    return(__make_transfer__(im, psf_params = psf_params, mode = 'psf', dimension = 2).squeeze())
+    return __make_transfer__(im, psf_params = psf_params, mode = 'psf', dimension = 2).squeeze()
 
 def jinc(mysize=[256,256],myscale=None):
     """

NanoImagingPack/transformations.py
@@ -122,9 +122,10 @@ def ft2d(im, shift_after = True, shift_before = True, ret ='complex', s = None, 
     """
     if np.ndim(im)<2:
         print('Too few dimensions')
-        return(im)
+        raise ValueError('Too few dimensions for ft2d')
+#        return im
     else:
-        return(ft(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-2, -1), s = s, norm = norm))
+        return ft(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-2, -1), s = s, norm = norm)
 
 
 def ift2d(im, shift_after = True, shift_before = True, ret ='complex', s = None, norm = "ortho"):
@@ -134,9 +135,10 @@ def ift2d(im, shift_after = True, shift_before = True, ret ='complex', s = None,
     
     if np.ndim(im)<2:
         print('Too few dimensions')
-        return(im)
+        raise ValueError('Too few dimensions for ift2d')
+#        return(im)
     else:
-        return(ift(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-2, -1), s = s, norm = norm))
+        return ift(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-2, -1), s = s, norm = norm)
 
 
 def ft3d(im, shift_after = True, shift_before = True, ret ='complex', s = None, norm = "ortho"):
@@ -145,9 +147,10 @@ def ft3d(im, shift_after = True, shift_before = True, ret ='complex', s = None, 
     """
     if np.ndim(im)<3:
         print('Too few dimensions')
-        return(im)
+        raise ValueError('Too few dimensions for ft3d')
+#        return(im)
     else:
-        return(ft(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-3, -2, -1), s = s, norm = norm))
+        return ft(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-3, -2, -1), s = s, norm = norm)
 
 
 def ift3d(im, shift_after = True, shift_before = True, ret ='complex', s = None, norm = "ortho"):
@@ -157,9 +160,10 @@ def ift3d(im, shift_after = True, shift_before = True, ret ='complex', s = None,
     
     if np.ndim(im)<3:
         print('Too few dimensions')
-        return(im)
+        raise ValueError('Too few dimensions for ift3d')
+#        return(im)
     else:
-        return(ift(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-3, -2, -1), s = s, norm = norm))
+        return ift(im, shift_after= shift_after, shift_before= shift_before, ret = ret, axes = (-3, -2, -1), s = s, norm = norm)
 
 
 # now the rft abbreviations:
@@ -349,6 +353,7 @@ def ft(im, shift_after = True, shift_before = True, ret ='complex', axes = None,
 
     """
     #create axes list
+    pxs = im.pixelsize
     axes=__checkAxes__(axes,im)
 
     if shift_before == True:
@@ -358,7 +363,7 @@ def ft(im, shift_after = True, shift_before = True, ret ='complex', axes = None,
     im=np.fft.fftn(im, axes=axes, s=s, norm=norm)
     if shift_after == True:
         im=np.fft.fftshift(im, axes=axes)  # corner freq to mid freq
-    return image.image(__ret_val__(im, ret))
+    return image.image(__ret_val__(im, ret), pixelsize=pxs)
 
 def ift(im, shift_after = True, shift_before = True, ret ='complex', axes = None, s = None, norm =  'ortho'):
     """
@@ -387,6 +392,7 @@ def ift(im, shift_after = True, shift_before = True, ret ='complex', axes = None
 
     """
 
+    pxs = im.pixelsize
     axes=__checkAxes__(axes,im)
 
     if shift_before == True:
@@ -396,7 +402,7 @@ def ift(im, shift_after = True, shift_before = True, ret ='complex', axes = None
     im=np.fft.ifftn(im, axes=axes, s=s, norm=norm)
     if shift_after == True:
         im=np.fft.fftshift(im, axes=axes)  # corner freq to mid freq
-    return image.image(__ret_val__(im, ret))
+    return image.image(__ret_val__(im, ret), pixelsize=pxs)
 
 
 def irft(im, s,shift_after = False,shift_before = False, ret ='complex', axes = None,  norm = None):
@@ -422,6 +428,7 @@ def irft(im, s,shift_after = False,shift_before = False, ret ='complex', axes = 
 
     """
     # create axis, shift_ax and real_ax
+    pxs = im.pixelsize
     axes=__checkAxes__(axes,im)
     real_axis = max(axes)  # always the last axis is the real one   as Default
 
@@ -433,7 +440,7 @@ def irft(im, s,shift_after = False,shift_before = False, ret ='complex', axes = 
     im=np.fft.irfftn(im, axes=axes, s=s, norm=norm).astype(image.defaultDataType)
     if shift_after == True:
         im=np.fft.fftshift(im, axes=axes)  # corner to mid
-    return image.image(__ret_val__(im, ret))
+    return image.image(__ret_val__(im, ret), pixelsize=pxs)
 
 
 def rft(im, shift_after = False, shift_before = False, ret = 'complex', axes = None,  s = None, norm = None):
@@ -465,6 +472,7 @@ def rft(im, shift_after = False, shift_before = False, ret = 'complex', axes = N
 
     """
     #create axes list
+    pxs = im.pixelsize
     axes=__checkAxes__(axes,im)
     real_axis = max(axes)  # always the last axis is the real one   as Default
 
@@ -481,9 +489,9 @@ def rft(im, shift_after = False, shift_before = False, ret = 'complex', axes = N
         if shift_after == True:
             shift_ax = [i for i in axes if i != real_axis]
             im=np.fft.fftshift(im, axes=shift_ax)  # corner freq to mid freq
-        return image.image(__ret_val__(im, ret))
+        return image.image(__ret_val__(im, ret), pixelsize=pxs)
 
-def resample(img,factors=[2.0, 2.0]):
+def resample(img, factors=[2.0, 2.0]):
     """
     resamples an image by an RFT (or FFT for complex data), extracting a ROI and performing an inverse RFT. The sum of values is kept constant.
 