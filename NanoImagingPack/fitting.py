Bug fixes (cosSinTheta was wrong at the center), and splitting the PSF calculation further down regarding Zernikes. Added zernikeStack.

NanoImagingPack/fitting.py
@@ -28,11 +28,11 @@ def fit_gauss2D(dat, DBG = False, title = '2D Gauss fit', startPos=None, startSi
      """
 
     def remove_nan(arr):
-        return(arr[~np.isnan(arr)])
+        return arr[~np.isnan(arr)]
 
     def check_angle_bonds(dat,p):
         if (p[5] <0) or (p[5]>180):
-            return(1e6)
+            return 1e6
         else:
             return remove_nan(np.ravel(gaussian(*p)(*np.indices(dat.shape))-dat))
 
@@ -42,15 +42,15 @@ def fit_gauss2D(dat, DBG = False, title = '2D Gauss fit', startPos=None, startSi
         (m1, m2) = startPos
 
     if startSigma is None:
-        s1 = np.size(dat,axis=-1)/2
-        s2 = np.size(dat,axis=-2)/2
+        s1 = np.size(dat, axis=-1)/2
+        s2 = np.size(dat, axis=-2)/2
     else:
         (s1, s2) = startSigma
 
-    x,y = np.meshgrid(np.arange(0,np.size(dat,axis =-1),1),np.arange(0,np.size(dat,axis =-2),1))
-    guess =np.asarray([np.max(remove_nan(dat)),m1,m2,s1,s2,90,np.min(remove_nan(dat))])
+    x,y = np.meshgrid(np.arange(0,np.size(dat, axis =-1),1),np.arange(0,np.size(dat, axis =-2),1))
+    guess =np.asarray([np.max(remove_nan(dat)), m1, m2, s1, s2, 90, np.min(remove_nan(dat))])
 
-    deviation = lambda p: remove_nan(np.ravel(gaussian(*p)(np.indices(dat.shape)[1],np.indices(dat.shape)[0])-dat))
+    deviation = lambda p: remove_nan(np.ravel(gaussian(*p)(np.indices(dat.shape)[1], np.indices(dat.shape)[0])-dat))
     p, success = scp.optimize.leastsq(deviation, guess)
     if DBG:
         print('Initial guess parameters:')
@@ -92,12 +92,12 @@ def fit_FWHM(dat, Verbose = True, startPos=None, startSigma=None):
     FWHM_y = 2*(sigma_y * np.sqrt(-np.log(0.5)*2))
 
     if Verbose:
-        print("FWHM x: " + str(FWHM_x) + " pixels")
-        print("FWHM y: " + str(FWHM_y) + " pixels")
+        print("FWHM x: " + str(np.abs(FWHM_x)) + " pixels")
+        print("FWHM y: " + str(np.abs(FWHM_y)) + " pixels")
     FWHM_x *= dat.pixelsize[-1]
     FWHM_y *= dat.pixelsize[-2]
     if Verbose:
-        print("FWHM x: " + str(FWHM_x) + " " + dat.unit)
-        print("FWHM y: " + str(FWHM_y) + " " + dat.unit)
+        print("FWHM x: " + str(np.abs(FWHM_x)) + " " + dat.unit)
+        print("FWHM y: " + str(np.abs(FWHM_y)) + " " + dat.unit)
         v5(catE(dat, fitted))
     return FWHM_x, FWHM_y, amplitude

NanoImagingPack/image.py
@@ -2146,7 +2146,7 @@ class image(np.ndarray):
                          if output is None else output)
                         for result, output in zip(results, outputs))
 
-        if method == 'reduce' and isinstance(inputs[0],image) and inputs[0].pixelsize is not None:
+        if method == 'reduce' and isinstance(inputs[0], image) and inputs[0].pixelsize is not None:
             keepdims = kwargs['keepdims']
             for i, output_ in enumerate(results):
                 if isinstance(output_, image):
@@ -2164,7 +2164,8 @@ class image(np.ndarray):
 
         if method=="__call__":
             for i, output_ in enumerate(results):
-                output_.pixelsize = util.longestPixelsize(inputs)
+                if isinstance(output_, image):
+                    output_.pixelsize = util.longestPixelsize(inputs)
 
         # if results and isinstance(results[0], image):
         #     results[0].info = info
@@ -2224,7 +2225,7 @@ class image(np.ndarray):
         #             pxs[-i-1] = p[-i-1]
         # self.pixelsize = pxs
         if isinstance(obj, image) and obj.pixelsize is not None:
-            self.pixelsize = obj.pixelsize.copy()
+            self.pixelsize = list(obj.pixelsize).copy()
         else:
             self.pixelsize = __DEFAULTS__['IMG_PIXELSIZES'] # which is None
 #        self.dim_description = getattr(obj,'dim_description', {'d0': [],'d1': [],'d2': [],'d3': [],'d4': [],'d5': []})

NanoImagingPack/microscopy.py
@@ -11,7 +11,7 @@ Also SIM stuff
 """
 
 import numpy as np
-from .util import zernike, expanddim, midValAsg, zeros, shapevec
+from .util import zernike, expanddim, midValAsg, zeros, shapevec, nollIdx2NM
 from .transformations import rft,irft,ft2d
 from .coordinates import rr, phiphi, px_freq_step, ramp1D, cosSinTheta
 from .config import PSF_PARAMS, __DEFAULTS__
@@ -70,10 +70,22 @@ def __setPol__(im, psf_params= None):
         raise ValueError('Wrong Polarization: "lin", "lin_x", "lin_y", "azimuthal", "radial", "circular", "elliptic" or tuple or list of polarization maps (polx,poly)')
     return(tuple(pol))
 
+def zernikeStack(im, psf_params=None, nzernikes=15):
+    """
+    create a stack of Zernike base aberrations including the scalar pupil.
+    """
+    psf_params = getDefaultPSF_PARAMS(psf_params)
+    r = pupilRadius(im, psf_params)
+    myzernikes = zeros((nzernikes, im.shape[-2], im.shape[-1]))  # + 1j * nip.zeros((nzernikes, Po.shape[-2], Po.shape[-1]))
+    for noll in range(0, nzernikes):
+        n, m = nollIdx2NM(noll + 1)
+        myzernikes[noll, :, :] = zernike(r, m, n) * np.pi
+    myzernikes.name = "Zernike Polynomials"
+    return myzernikes
 
 def aberrationMap(im, psf_params= None):
     """
-    create an aberration phase map (based on Zernike polynomials) or a transmission matrix (aperture) for PSF generation
+    create an aberration phase map (based on Zernike polynomials) for PSF generation
 
     uses:
         PSF_PARAMS().aberration_strength = None;
@@ -87,7 +99,7 @@ def aberrationMap(im, psf_params= None):
                         or phasemap
 
                         or string
-                                piston     -> (Z0,0)
+                                piston     -> (Z00)
                                 tiltY      -> (Z-11)
                                 tiltX      -> (Z11)
                                 astigm     -> (Z-22)
@@ -104,7 +116,6 @@ def aberrationMap(im, psf_params= None):
                                 vquadfoil  -> (Z44)
 
                     can be also lists or tuples -> Then everything will summed up
-    transmission:   The transmission map
     """
     psf_params = getDefaultPSF_PARAMS(psf_params)
     zernike_para = {'piston': (0,0),'tiltY': (-1,1),'tiltX': (1,1),'astigm': (-2,2),'defoc': (0,2),'vastig': (2,2),'vtrefoil': (-3,3),'vcoma': (-1,3),
@@ -128,13 +139,13 @@ def aberrationMap(im, psf_params= None):
     #     else:
     #         raise ValueError('Wrong transmission -> must be 2D image or array of the same xy-dimension as the image')
     if strength is not None and aberration is not None:
-        if isinstance(im, image):
-            pxs=im.px_freq_step()[-2:]
-        else:
-            pxs = __DEFAULTS__['IMG_PIXELSIZES']
-            pxs = px_freq_step(im, pxs)[-2:]
-
-        r = rr(shape, scale=pxs) * psf_params.wavelength / psf_params.NA
+        # if isinstance(im, image):
+        #     pxs=im.px_freq_step()[-2:]
+        # else:
+        #     pxs = __DEFAULTS__['IMG_PIXELSIZES']
+        #     pxs = px_freq_step(im, pxs)[-2:]
+        # r = rr(shape, scale=pxs) * psf_params.wavelength / psf_params.NA  # BAD !
+        r = pupilRadius(im, psf_params)
         if isinstance(strength, numbers.Real):
             strength = [strength]
         if type(aberration) == str or isinstance(aberration,np.ndarray):
@@ -157,9 +168,12 @@ def aberrationMap(im, psf_params= None):
     return aberration_map
 
 def propagatePupil(pupil, sizeZ, psf_params = None, mode = 'Fourier', doDampPupil=False):
+    return pupil * propagationStack(pupil, sizeZ, psf_params, mode, doDampPupil)
+
+def propagationStack(pupil, sizeZ, psf_params = None, mode = 'Fourier', doDampPupil=False):
     psf_params = getDefaultPSF_PARAMS(psf_params)
     myshape = (sizeZ,) + shapevec(pupil)[-2:]
-    return pupil * __make_propagator__(pupil, psf_params, doDampPupil, shape=myshape)
+    return __make_propagator__(pupil, psf_params, doDampPupil, shape=myshape)
 
 def __make_propagator__(im, psf_params = None, doDampPupil=False, shape=None):
     """
@@ -297,7 +311,6 @@ def defocusPhase(cos_alpha, defocus=None, psf_params = None):
     defocusPhase = psf_params.n / psf_params.wavelength * cos_alpha * defocus
     return 2.0 * np.pi * defocusPhase
 
-
 def aplanaticFactor(cos_alpha, aplanar = 'emission'):
     """
     calculated the aplanatic factor according to the argument aplanar
@@ -331,7 +344,7 @@ def aplanaticFactor(cos_alpha, aplanar = 'emission'):
 
 def aberratedPupil(im, psf_params = None):
     """
-    Compute a scalar aberrated pupil
+    Compute a scalar aberrated pupil, possibly with the jinc-Trick. Aplanatic factors are included, but not the pupil propagation stack
 
     The following influences will be included:
         1.)     Mode of plane field generation (from sinc or circle)
@@ -379,7 +392,7 @@ def simLens(im, psf_params = None):
     psf_params = getDefaultPSF_PARAMS(psf_params)
 
     plane = aberratedPupil(im, psf_params)  # scalar aberrated (potentially defocussed) pupil
-    shape = shapevec(im)[-2:]   # works also for tensorflow objects
+#    shape = shapevec(im)[-2:]   # works also for tensorflow objects
 
     # expand to 4 Dims, the 4th will contain the electric fields
     plane = plane.cat([plane, plane], -4)

NanoImagingPack/transformations.py
@@ -330,7 +330,7 @@ def pad(array, pad_width, mode, **kwargs):
     return image.image(np.pad(array, pad_width, mode, **kwargs), pixelsize=array.pixelsize)
 
 def stack(arrays, axis=0, out=None):
-    pixelsize = arrays[0].pixelsize.copy()
+    pixelsize = util.longestPixelsize(arrays) # arrays[0].pixelsize.copy()
     res = image.image(np.stack(arrays, axis, out), pixelsize=pixelsize)
     if axis == 0:
         res.pixelsize = [None] + res.pixelsize

NanoImagingPack/util.py
@@ -125,8 +125,56 @@ def inrange(arr, ran):
         ran = ran[::-1]
     return((arr>ran[0])&(arr<ran[1]))
 
-def zernike(r,m,n, radial = False):
+# The code below is from https://github.com/david-hoffman/pyOTF/blob/master/zernike.py
+noll_mapping = np.array([
+    1, 3, 2, 5, 4, 6, 9, 7, 8, 10, 15, 13, 11, 12, 14, 21, 19, 17, 16,
+    18, 20, 27, 25, 23, 22, 24, 26, 28, 35, 33, 31, 29, 30, 32, 34,
+    36, 45, 43, 41, 39, 37, 38, 40, 42, 44, 55, 53, 51, 49, 47, 46,
+    48, 50, 52, 54, 65, 63, 61, 59, 57, 56, 58, 60, 62, 64, 66, 77,
+    75, 73, 71, 69, 67, 68, 70, 72, 74, 76, 78, 91, 89, 87, 85, 83,
+    81, 79, 80, 82, 84, 86, 88, 90, 105, 103, 101, 99, 97, 95, 93,
+    92, 94, 96, 98, 100, 102, 104, 119, 117, 115, 113, 111, 109,
+    107, 106, 108, 110, 112, 114, 116, 118, 120
+])
 
+# reverse mapping of noll indices
+noll_inverse = noll_mapping.argsort()
+
+def nollIdx2NM(noll):
+    """Convert from Noll's indices to radial degree and azimuthal degree"""
+    noll = np.asarray(noll)
+    if not np.issubdtype(noll.dtype, np.signedinteger):
+        raise ValueError("input is not integer, input = {}".format(noll))
+    if not (noll > 0).all():
+        raise ValueError(
+            "Noll indices must be greater than 0, input = {}".format(noll))
+    # need to subtract 1 from the Noll's indices because they start at 1.
+    p = noll_inverse[noll - 1]
+    n = np.ceil((-3 + np.sqrt(9 + 8 * p)) / 2)
+    m = 2 * p - n * (n + 2)
+    return n.astype(int), m.astype(int)
+
+
+def NM2NollIdx(n, m):
+    """Convert from radial and azimuthal degrees to Noll's index"""
+    n, m = np.asarray(n), np.asarray(m)
+    # check inputs
+    if not np.issubdtype(n.dtype, int):
+        raise ValueError(
+            "Radial degree is not integer, input = {}".format(n))
+    if not np.issubdtype(m.dtype, int):
+        raise ValueError(
+            "Azimuthal degree is not integer, input = {}".format(m))
+    if ((n - m) % 2).any():
+        raise ValueError(
+            "The difference between radial and azimuthal degree isn't mod 2")
+    # do the mapping
+    p = (m + n * (n + 2)) / 2
+    noll = noll_mapping[p.astype(int)]
+    return noll
+# end of code from https://github.com/david-hoffman/pyOTF/blob/master/zernike.py
+
+def zernike(r, m, n, radial = False):
     """
         compute the zerincke polynomial Z^m_n(r, phi)
 
@@ -147,11 +195,11 @@ def zernike(r,m,n, radial = False):
     m = np.abs(m)
     n = np.abs(n)
     if np.mod((n-m),2) ==1:
-        return(np.zeros(r.shape))
+        return zeros(r.shape, pixelsize=r.pixelsize)
     else:
-        k = np.arange(0, (n-m)//2+1,1)
+        k = np.arange(0, (n-m)//2+1, 1)
         zer_coeff = (-1)**k*factorial(n-k)/(factorial(k)*factorial((n+m)/2-k)*factorial((n-m)/2-k))
-        zer = np.zeros(r.shape)
+        zer = zeros(r.shape, pixelsize=r.pixelsize)
         for c, k_el in zip(zer_coeff, k):       # on purpose used a for loop here, as it is indeed faster
             zer += c*r**(n-2*k_el)
     return zer*fact
@@ -802,10 +850,10 @@ def ones(s,dtype=None,order='C'):
         s=s.shape
     return image.image(np.ones(s,dtype,order))
 
-def zeros(s,dtype=None,order='C'):
+def zeros(s, dtype=None, order='C', pixelsize=None):
     if isnp(s):
-        s=s.shape
-    return image.image(np.zeros(s,dtype,order))
+        s = s.shape
+    return image.image(np.zeros(s, dtype, order), pixelsize=pixelsize)
 
 
 def __cast__(arr, orig_arr=None):