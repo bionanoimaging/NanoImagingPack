Bug fix in the returned variance when a range was selected. This affected cal_readnoise.

NanoImagingPack/microscopy.py
@@ -190,7 +190,8 @@ def aberrationMap(im, psf_params= None):
 
 def propagatePupil(pupil, sizeZ, distZ=None, psf_params = None, mode = 'Fourier', doDampPupil=False):
     """
-    Propagates a pupil plane to a 3D stack of pupils.
+    Propagates a pupil plane to a 3D stack of pupils. The result is still in Fourier space!
+    To obtain a 3D stack, you need to do an ift2d().
     :param pupil: pupil amplitude to propagate
     :param sizeZ: Number of slices to propagate
     :param distZ: Distance between the slices (in units of the XY pixel information of the pupil)
@@ -213,12 +214,13 @@ def propagatePupil(pupil, sizeZ, distZ=None, psf_params = None, mode = 'Fourier'
 
 def propagationStack(pupil, sizeZ, distZ=None, psf_params = None, mode = 'Fourier', doDampPupil=False):
     """
-
-    :param pupil:
-    :param sizeZ:
-    :param psf_params:
+    generates a 3D stack in Fourier space containing the propagators. The pixelsize needs to be defined for the pupil (in real space pixels).
+    :param pupil:    Fourier plane to propagate
+    :param sizeZ:    Size to propagate along Z. XY sizes are inferred from the pupil size
+    :param distZ:     pixelsize along Z for propagation
+    :param psf_params:  structure of PSF parameters
     :param mode:
-    :param doDampPupil:
+    :param doDampPupil:  defines wheter a damped pupil shall be used for the propagation
     :return:
 
     Example:
@@ -240,6 +242,7 @@ def __make_propagator__(im, psf_params = None, doDampPupil=False, shape=None, di
 
     :param im:              input image
     :param psf_params:      psf params structure
+    :param distZ:           pixelsize along Z for propagation
 
                             TODO: Include chirp Z transform method
     :return:                Returns the phase propagation matrix (exp^i*delta_phi)
@@ -1440,8 +1443,18 @@ def cal_readnoise(fg,bg,numBins=100, validRange=None, CameraName=None, correctBr
     :param numBins: Number of bins for the fit
     :param validRange: If provided, the fit will only be performed over this range (from,to) of mean values. The values are inclusived borders
     :param doPlot: Plot the mean-variance curves
-    :return: tuple of fit results (offset [adu], gain [electrons / adu], readnoise [e- RMS])
+    :return: tuple of fit results (offset [adu], gain [electrons / adu], readnoise [e- RMS], hotpixemap)
     to scale your images into photons use: (image-offset) * gain
+
+    Example:
+    import NanoImagingPack as nip
+    import numpy as np
+    obj = nip.repmat(nip.readim(),[10,1,1])
+    RN = 3.5
+    gain = 2.3
+    nimg = (nip.noise.poisson(obj) + np.random.normal(obj*0.0,RN)) / gain
+    bgimg = np.random.normal(obj*0.0,RN) / gain
+    (offset, myGain, myReadnoise, hotPixelMap) =nip.cal_readnoise(nimg, bgimg, validRange=[20,80])
     """
     # a_nobg = a * 1.0 - meanbg
     AxisFontSize=16